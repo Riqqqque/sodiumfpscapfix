3e171a51c349ad081bf621b2006947dd4046e9b7
Riqqqque <goldenroses6200@gmail.com>
2026-04-13 01:11:21 -0600

    Fix 26.1.2 Fabric startup mixin

--- common-src/26.1.2/java/com/rique/sodiumfpscapfix/mixin/MinecraftMixin.java
+++ common-src/26.1.2/java/com/rique/sodiumfpscapfix/mixin/MinecraftMixin.java
@@ -1,15 +1,27 @@
 package com.rique.sodiumfpscapfix.mixin;
 
-import com.rique.sodiumfpscapfix.FpsCapConstants;
+import com.mojang.blaze3d.platform.FramerateLimitTracker;
+import net.minecraft.client.FramerateLimiter;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
-    @ModifyConstant(method = "runTick", constant = @Constant(intValue = 260))
-    private int sodiumfpscapfix$raiseFpsCutoff(int original) {
-        return FpsCapConstants.UNLIMITED_CUTOFF;
+    private static final int VANILLA_UNCAPPED_MARKER = 260;
+
+    @Shadow
+    private FramerateLimitTracker framerateLimitTracker;
+
+    @Inject(method = "runTick(Z)V", at = @At("TAIL"))
+    private void sodiumfpscapfix$limitHighFpsCaps(boolean renderLevel, CallbackInfo ci) {
+        int fpsCap = this.framerateLimitTracker.getFramerateLimit();
+
+        if (fpsCap >= VANILLA_UNCAPPED_MARKER) {
+            FramerateLimiter.limitDisplayFPS(fpsCap);
+        }
     }
 }
