8153d0761200c15aa1cba94f5262aca1481eaa40
Riqqqque <goldenroses6200@gmail.com>
2026-05-05 22:38:03 -0600

    Fix FPS cap range hooks

--- common-src/1.20.1/java/com/rique/sodiumfpscapfix/FpsCapConstants.java
+++ common-src/1.20.1/java/com/rique/sodiumfpscapfix/FpsCapConstants.java
@@ -1,7 +1,7 @@
 package com.rique.sodiumfpscapfix;
 
 public final class FpsCapConstants {
-    public static final int MIN_FPS_CAP = 1;
+    public static final int MIN_FPS_CAP = 10;
     public static final int MAX_FPS_CAP = 1_000_000;
     public static final int UNLIMITED_CUTOFF = MAX_FPS_CAP + 1;
     public static final int TEXT_BOX_WIDTH = 92;
--- common-src/1.21.11/java/com/rique/sodiumfpscapfix/mixin/OptionsMixin.java
+++ common-src/1.21.11/java/com/rique/sodiumfpscapfix/mixin/OptionsMixin.java
@@ -1,35 +1,45 @@
 package com.rique.sodiumfpscapfix.mixin;
 
 import com.rique.sodiumfpscapfix.FpsCapPersistence;
-import com.rique.sodiumfpscapfix.FpsCapSupport;
-import com.rique.sodiumfpscapfix.UnlockedFpsCapValueSet;
+import com.rique.sodiumfpscapfix.FpsCapConstants;
 import net.minecraft.client.OptionInstance;
 import net.minecraft.client.Options;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(Options.class)
 public abstract class OptionsMixin {
     @Shadow
     public abstract OptionInstance<Integer> framerateLimit();
 
-    @ModifyArgs(
+    @ModifyConstant(
             method = "<init>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/OptionInstance;<init>(Ljava/lang/String;Lnet/minecraft/client/OptionInstance$TooltipSupplier;Lnet/minecraft/client/OptionInstance$CaptionBasedToString;Lnet/minecraft/client/OptionInstance$ValueSet;Lcom/mojang/serialization/Codec;Ljava/lang/Object;Ljava/util/function/Consumer;)V"
-            )
+            slice = @Slice(
+                    from = @At(value = "CONSTANT", args = "stringValue=options.framerateLimit")
+            ),
+            constant = @Constant(intValue = 26, ordinal = 0),
+            require = 1
     )
-    private void sodiumfpscapfix$unlockFpsLimit(Args args) {
-        if ("options.framerateLimit".equals(args.get(0))) {
-            args.set(3, UnlockedFpsCapValueSet.wrap(args.get(3)));
-            args.set(4, FpsCapSupport.codec());
-        }
+    private int sodiumfpscapfix$unlockFpsLimitRange(int original) {
+        return FpsCapConstants.MAX_FPS_CAP / 10;
+    }
+
+    @ModifyConstant(
+            method = "<init>",
+            slice = @Slice(
+                    from = @At(value = "CONSTANT", args = "stringValue=options.framerateLimit")
+            ),
+            constant = @Constant(intValue = 260, ordinal = 0),
+            require = 1
+    )
+    private int sodiumfpscapfix$unlockFpsLimitCodecRange(int original) {
+        return FpsCapConstants.MAX_FPS_CAP;
     }
 
     @Inject(method = "<init>", at = @At("RETURN"))
